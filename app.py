56b5ba5168f758ba3c7da7c7b0774f41b4d56d82
Johann <50180558+johannpiedras@users.noreply.github.com>
2021-04-06 10:15:48 -0400

    Updating Templates
    Uploading html files for the Flask Application.

--- app.py
+++ app.py
@@ -1,7 +1,63 @@
-from flask import Flask, render_template
+###
+### MUST USE FOR APP
+###
+from flask import Flask, render_template, request, redirect
+
+
+###
+### Extra shit
+###
+import os
+import re
+import json
+import string
+from pathlib import Path
+import smtplib
+
+
 
 app = Flask(__name__)
 
+# Adding variables that I will need
+subscribers = []
+
 @app.route('/')
-def index():
-    return render_template('index.html')
+def index(): # {{ url_for('index')}}
+	return render_template('index.html')
+
+
+@app.route('/about')
+def about(): # {{ url_for('about')}}
+	names = ["Johann", "Ron", "Loay", "Zarak", "Ava", "Daniel", "Zeb", "Jay", "Jonatas", "Amir"]
+	return render_template('about.html', names = names)
+
+
+@app.route('/contact')
+def contact(): # {{ url_for('index')}}
+	return render_template('contact.html')
+
+@app.route('/form', methods =['POST'])
+def form(): # {{ url_for('index')}}
+	firstNAME = request.form.get("first_name")
+	lastNAME = request.form.get('last_name')
+	EMAIL =  request.form.get('email')
+
+	# SETTING UP EMAIL STUFF
+	my_email = 'johannpiedras.email@gmail.com'
+	my_password = 'testingEMAIL1995'
+
+	message = "You have been contacted Online Science Tutor bu Johann Piedras. Thank you!"
+	server = smtplib.SMTP("smtp.gmail.com" , 587)
+	server.starttls()
+	server.login(my_email, my_password)
+	server.sendmail(my_email, EMAIL, message)
+	# my_email has my email
+	# EMAIL is what was given by the user
+
+	if not firstNAME or not lastNAME or not EMAIL:
+		error_statement = "All form fields required please. Dont be SNEAKY"
+		return render_template("fail.html", error_statement = error_statement,  first_name = firstNAME, last_name = lastNAME, email = EMAIL, subscribers = subscribers)
+
+	subscribers.append(f" {firstNAME} {lastNAME} | {EMAIL}")
+	title = "Thank you!"
+	return render_template('form.html', title = title, first_name = firstNAME, last_name = lastNAME, email = EMAIL, subscribers = subscribers)
